f0c4d4d20624cd19576657801b8f8c5cdda4c81c
badyanov <d.badyanov@outlook.com>
2020-10-30 22:51:16 +0300

    Исправлена опечатка пакета firuges -> figures

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -2,8 +2,8 @@ package ru.job4j.chess;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import ru.job4j.chess.firuges.Cell;
-import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.figures.Cell;
+import ru.job4j.chess.figures.black.BishopBlack;
 
 public class LogicTest {
 
